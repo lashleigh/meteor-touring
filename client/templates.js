21235dbf6dfce1d4b6fe79784c739014f61ef0d0
Ashleigh Baumgardner <LABaumgardner@gmail.com>
2012-04-25 10:41:24 -0700

    whoops, this goes with previous comment

--- client/templates.js
+++ client/templates.js
@@ -121,24 +121,26 @@ Template.day.events = {
     e.stopPropagation();
     var day = this;
     if(Days.find({order: {$gt: day.order}}).count() !== 0) {
-      if(!!day.polyline) {
+      if(day.polyline) {
         var path = decodePath(day.polyline);
-        var midpoint = path[Math.floor(path.length/2)];
-        munge_update({order: {$gte: day.order+1}}, {$inc: {order: 1}}, {multi: true});
-        munge_update(day._id, {$set: {waypoints: []}}); //TODO Unfortunate but how else would I keep the waypoint order straight?
-        var d = munge_insert({lat: midpoint.lat(), lng:midpoint.lng(), order: day.order+1});
-        make_current(d);
-        calc_route_with_stopover(Days.findOne(d));
-      } else {
-        console.log('the day doesnt have a polyline yet...');
-        var next_day = Days.findOne({order: day.order + 1});
-        var half_lat = day.lat - (day.lat - next_day.lat) / 2;
-        var half_lng = day.lng - (day.lng - next_day.lng) / 2;
-        munge_update({order: {$gte: day.order+1}}, {$inc: {order: 1}}, {multi: true});
-        munge_update(day._id, {$set: {waypoints: []}}); //TODO Unfortunate but how else would I keep the waypoint order straight?
-        var d = munge_insert({lat: half_lat, lng:half_lng, order: day.order+1});
-        make_current(d);
-        calc_route_with_stopover(Days.findOne(d));
+        if(path.length > 2) {
+          var midpoint = path[Math.floor(path.length/2)];
+          munge_update({order: {$gte: day.order+1}}, {$inc: {order: 1}}, {multi: true});
+          munge_update(day._id, {$set: {waypoints: []}}); //TODO Unfortunate but how else would I keep the waypoint order straight?
+          var d = munge_insert({lat: midpoint.lat(), lng:midpoint.lng(), order: day.order+1});
+          make_current(d);
+          calc_route_with_stopover(Days.findOne(d));
+        } else {
+          console.log('the day doesnt have a polyline yet...');
+          var next_day = Days.findOne({order: day.order + 1});
+          var half_lat = day.lat - (day.lat - next_day.lat) / 2;
+          var half_lng = day.lng - (day.lng - next_day.lng) / 2;
+          munge_update({order: {$gte: day.order+1}}, {$inc: {order: 1}}, {multi: true});
+          munge_update(day._id, {$set: {waypoints: []}}); //TODO Unfortunate but how else would I keep the waypoint order straight?
+          var d = munge_insert({lat: half_lat, lng:half_lng, order: day.order+1});
+          make_current(d);
+          calc_route_with_stopover(Days.findOne(d));
+        }
       }
     } else {
       console.log('inserting on the last day doesnt make sense');
